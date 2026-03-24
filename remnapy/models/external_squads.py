bec1c72b321886a069e1ba33aa346f447f7a8455
Ilay <isnoups@gmail.com>
2026-03-24 08:11:25 +0500

    fix: imports

--- remnapy/models/external_squads.py
+++ remnapy/models/external_squads.py
@@ -5,7 +5,7 @@ from uuid import UUID
 
 from pydantic import BaseModel, Field
 
-from remnapy.models import CustomRemarksDto, HwidSettingsDto
+from .subscriptions_settings import CustomRemarksDto, HwidSettingsDto
 
 
 class TemplateType(StrEnum):
